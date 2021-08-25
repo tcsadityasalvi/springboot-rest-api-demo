d1ab1c31a0fe6016bc6d7bd739936331c6034382
tcsadityasalvi <tcsadityasalvi@gmail.com>
2021-08-25 05:15:47 +0000

    added API

--- src/main/java/com/tcs/springbootdemo/UserController.java
+++ src/main/java/com/tcs/springbootdemo/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -13,15 +14,25 @@ public class UserController {
 	
 	@Autowired  //DI
 	IUserService userService;
-	@GetMapping("/getuser")
+	@GetMapping("/user")
 	private String getUser() {
 		System.out.println("called");
-		return "hello";
+		return "hello";}
+		
+		@GetMapping("/user")
+		private Iterable<User> getUser1() {
+			return userService.getAllUsers();
 		
 	}
 	
 	@PostMapping("/user")
 	private void saveUser(@RequestBody User user) {
+		System.out.println(user.getFirstName());
+	}
+	
+	@PutMapping("/user") //METHOD+Path
+	private void updateUser(@RequestBody User user) {
+		userService.save(user);
 		System.out.println(user.getFirstName());
 	}
 
